12ec80b40629e77d0b37f54f12fe55423590f2c1
4tsu <sho4tsuji@gmail.com>
2022-07-12 19:50:41 +0900

    ✨ particleクラスの導入

--- 2d/particle.py
+++ 2d/particle.py
@@ -2,3 +2,16 @@
 ## シミュレーションを行う粒子・エージェントについてのコード
 # ===================================================
 
+# ---------------------------------------------------
+
+class Particle:
+    vx = 0
+    vy = 0
+
+    def __init__(self, x_init, y_init):
+        self.x = x_init
+        self.y = y_init
+    
+    def set_velocity(self, vx, vy):
+        self.vx = vx
+        self.vy = vy
